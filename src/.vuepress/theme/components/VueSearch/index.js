0345233895e7182d4f93b35616db9383ce9a5f96
Waro1234 <thaman663@gmail.com>
2022-02-08 00:15:05 +0100

    Update index.js

--- src/.vuepress/theme/components/VueSearch/index.js
+++ src/.vuepress/theme/components/VueSearch/index.js
@@ -4,10 +4,7 @@ const { path } = require('@vuepress/shared-utils')
  * @type {import('@vuepress/types').Plugin}
  */
 module.exports = (options) => ({
-  alias: {
-    '@SearchBox':
-      path.resolve(__dirname, 'SearchBox.vue')
-  },
+  
 
   define: {
     SEARCH_MAX_SUGGESTIONS: options.searchMaxSuggestions || 5,
